c013f8cd9217f4607a7dda20b61fba3830b0f53b
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-11-10 20:44:16 +0700

    first commit

---
+++ components/Library.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+const Library = () => {
+  const onClick = () => {
+    // Handle search
+  };
+  return <div className="flex flex-col">Library</div>;
+};
+
+export default Library;
