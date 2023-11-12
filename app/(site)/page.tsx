ad2fde84f94c15158aa23f5baf5b1858fb1e5f81
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-11-12 16:04:33 +0700

    update: homepage

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,7 @@
 import getSong from "@/actions/getSong";
 import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
-import PageContent from "@/components/PageContent";
+import PageContent from "@/app/(site)/components/PageContent";
 
 export const revalidate = 0;
 
