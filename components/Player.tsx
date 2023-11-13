55b61a24eba36bbbeca5103f6de7db1b9b903606
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-11-13 21:54:01 +0700

    update: title on mobile

--- components/Player.tsx
+++ components/Player.tsx
@@ -13,7 +13,7 @@ const Player = () => {
   if (!songUrl || !song || !player.activeId) return null;
 
   return (
-    <div className="fixed bottom-0 bg-black w-full py-2 h-[80px] px-4">
+    <div className="fixed bottom-0 bg-black w-full py-2  px-4">
       <PlayerContent key={songUrl} song={song} songUrl={songUrl} />
     </div>
   );
--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -4,6 +4,7 @@ import { AiFillStepBackward, AiFillStepForward } from "react-icons/ai";
 import { HiSpeakerXMark, HiSpeakerWave } from "react-icons/hi2";
 import { useEffect, useState } from "react";
 import useSound from "use-sound";
+import { motion } from "framer-motion";
 
 import { Song } from "@/types";
 import MediaItem from "./MediaItem";
@@ -73,47 +74,73 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
   const toggleMute = () => (volume === 0 ? setVolume(1) : setVolume(0));
 
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 h-full">
-      <div className="flex w-full">
-        <div className="flex items-center gap-x-4">
-          <MediaItem data={song} />
-          <LikeButton songId={song.id} />
-        </div>
-      </div>
-      <div className="flex md:hidden col-auto w-full justify-end items-center">
-        <div
-          onClick={handlePlay}
-          className="h-10 w-10 flex items-center justify-center bg-white p-1 cursor-pointer rounded-full"
+    <div className="flex flex-col">
+      <div className="flex md:hidden overflow-hidden">
+        <motion.div
+          initial={{ x: "-100%" }}
+          animate={{ x: "100vw" }}
+          transition={{ duration: 7, ease: "linear", repeat: Infinity }}
+          className="flex items-center"
         >
-          <Icon size={30} className="text-black" />
-        </div>
+          <p className="font-bold">{song.title}</p>
+          <p className="px-2">•</p>
+          <p className="text-neutral-400 text-xs">{song.author}</p>
+        </motion.div>
       </div>
-      <div className="hidden h-full md:flex justify-center items-center w-full max-w-[722px] gap-x-6">
-        <AiFillStepBackward
-          onClick={onPlayPrevious}
-          size={30}
-          className="text-neutral-400 cursor-pointer hover:text-white transition"
-        />
-        <div
-          onClick={handlePlay}
-          className="flex items-center justify-center h-10 w-10 rounded-full bg-white p-1 cursor-pointer"
-        >
-          <Icon size={30} className="text-black" />
+      <div className="grid grid-cols-2 md:grid-cols-3 h-full">
+        <div className="flex w-full">
+          <div className="flex items-center gap-x-4">
+            <MediaItem data={song} className="hidden" />
+            <LikeButton songId={song.id} />
+          </div>
         </div>
-        <AiFillStepForward
-          onClick={onPlayNext}
-          size={30}
-          className="text-neutral-400 cursor-pointer hover:text-white transition"
-        />
-      </div>
-      <div className="hidden md:flex w-full justify-end pr-2">
-        <div className="flex items-center gap-x-2 w-[120px]">
-          <VolumeIcon
-            onClick={toggleMute}
+        <div className="flex gap-1 md:hidden col-auto w-full justify-end items-center">
+          <div
+            onClick={onPlayPrevious}
+            className="h-9 w-9 flex items-center justify-center bg-white p-1 cursor-pointer rounded-full"
+          >
+            <AiFillStepBackward size={30} className="text-black" />
+          </div>
+          <div
+            onClick={onPlayNext}
+            className="h-9 w-9 flex items-center justify-center bg-white p-1 cursor-pointer rounded-full"
+          >
+            <AiFillStepForward size={30} className="text-black" />
+          </div>
+          <div
+            onClick={handlePlay}
+            className="h-10 w-10 flex items-center justify-center bg-green-400 p-1 cursor-pointer rounded-full"
+          >
+            <Icon size={30} className="text-black" />
+          </div>
+        </div>
+        <div className="hidden h-full md:flex justify-center items-center w-full max-w-[722px] gap-x-6">
+          <AiFillStepBackward
+            onClick={onPlayPrevious}
             size={30}
-            className="cursor-pointer"
+            className="text-neutral-400 cursor-pointer hover:text-white transition"
           />
-          <Slider value={volume} onChange={(value) => setVolume(value)} />
+          <div
+            onClick={handlePlay}
+            className="flex items-center justify-center h-10 w-10 rounded-full bg-white p-1 cursor-pointer"
+          >
+            <Icon size={30} className="text-black" />
+          </div>
+          <AiFillStepForward
+            onClick={onPlayNext}
+            size={30}
+            className="text-neutral-400 cursor-pointer hover:text-white transition"
+          />
+        </div>
+        <div className="hidden md:flex w-full justify-end pr-2">
+          <div className="flex items-center gap-x-2 w-[120px]">
+            <VolumeIcon
+              onClick={toggleMute}
+              size={30}
+              className="cursor-pointer"
+            />
+            <Slider value={volume} onChange={(value) => setVolume(value)} />
+          </div>
         </div>
       </div>
     </div>
