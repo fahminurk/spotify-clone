6539cd5e786bf27c710f9d18df48142ef7d048c9
Fahmi Nurkamil <fahminurk31@gmail.com>
2023-11-13 21:54:42 +0700

    update: fix bug

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -3,13 +3,15 @@ import useLoadImage from "@/hooks/useLoadImage";
 import { Song } from "@/types";
 import Image from "next/image";
 import React from "react";
+import { twMerge } from "tailwind-merge";
 
 interface MediaItemProps {
   data: Song;
   onClick?: (id: string) => void;
+  className?: string;
 }
 
-const MediaItem: React.FC<MediaItemProps> = ({ data, onClick }) => {
+const MediaItem: React.FC<MediaItemProps> = ({ data, onClick, className }) => {
   const imageUrl = useLoadImage(data);
 
   const handleClick = () => {
@@ -29,7 +31,12 @@ const MediaItem: React.FC<MediaItemProps> = ({ data, onClick }) => {
           alt="image"
         />
       </div>
-      <div className="flex flex-col gap-y-1 overflow-hidden">
+      <div
+        className={twMerge(
+          "md:flex flex-col gap-y-1 overflow-hidden",
+          className
+        )}
+      >
         <p className="text-white truncate">{data.title}</p>
         <p className="text-neutral-400 text-sm truncate">{data.author}</p>
       </div>
